20c83d4eabf0311ab036b79182c41c618e0ec814
DecoCrafttech <decocrafttech@gmail.com>
2024-06-30 09:56:18 +0530

    register and profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Page_header from './global-components/page-header'
+import MyAccount from './shop-components/my-account'
 
 const Profile = () => {
   return (
     <>
-        <Page_header headertitle="My profile" />
+        {/* <Page_header headertitle="My profile" /> */}
+        <MyAccount/>
+        
     </>
   )
 }
